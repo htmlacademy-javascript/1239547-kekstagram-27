44895b29bc264a5bf432142ec144b3cdf9d00b08
Natalia <yadykova.natalia@yandex.ru>
2022-11-02 21:57:16 +0300

    Изменение функции для отрисовки фотографий

--- js/picture.js
+++ js/picture.js
@@ -5,7 +5,7 @@ const renderPhoto = ({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   return pictureElement;
 };
 
